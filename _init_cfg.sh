821b5c13fcd4ccc50c9ac47e41b19dee31c848c6
SpicyChickenFLY <xixiayuchao@gmail.com>
2023-11-14 13:26:48 +0800

    init: udpate shell

--- _init_cfg.sh
+++ _init_cfg.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-$dot_files_dir="$HOME/.config/dot-files"
+dot_files_dir=$HOME/.config/dot-files
 
 create_dir_arr=("i3", "i3status", "rofi", "alacritty", "lazygit", "nvim")
 for dir in ${create_dir_arr[@]}; do
     ln -s "${dot_files_dir}/${dir}" "$HOME/.config/${dir}"
 done
 
-ln -s "${dot_files_dir}/xprofile" "$HOME/.xprofile"
-ln -s "${dot_files_dir}/zshrc" "$HOME/.zshrc"
-sed -i '/^export http/d' "$dot_files_dir/zshrc"
-
+cp "${dot_files_dir}/xprofile" "$HOME/.xprofile"
+cp "${dot_files_dir}/zshrc" "$HOME/.zshrc"
+sed -i '/^export http/d' "$HOME/.zshrc"
