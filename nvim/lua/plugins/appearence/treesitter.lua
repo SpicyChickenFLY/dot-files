1c958892c7701fc2cff914f98765c9b7f6a5a0b9
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-01-23 05:33:51 +0800

    nvim: disable TS highlight for specified language

--- nvim/lua/plugins/appearence/treesitter.lua
+++ nvim/lua/plugins/appearence/treesitter.lua
@@ -6,7 +6,15 @@ local defaults = {
     },
     highlight = {
         enable = true,
-        disable = function(_, buf) -- omit lang
+        disable = function(lang, buf) -- omit lang
+            local lang_table = { "scss", "css", "html", "http",
+                "jsdoc", "javascript", "typescript", "tsx", "vue" }
+            for _, parser in ipairs(lang_table) do
+                if lang == parser then
+                    return false
+                end
+            end
+
             local max_filesize = 100 * 1024 -- 100KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
