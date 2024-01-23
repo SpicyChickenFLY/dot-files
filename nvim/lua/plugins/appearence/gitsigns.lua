a8b0bce0a73e0538337523805f01f60ff38d2055
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-01-23 17:35:37 +0800

    nvim: update sign column

--- nvim/lua/plugins/appearence/gitsigns.lua
+++ nvim/lua/plugins/appearence/gitsigns.lua
@@ -17,7 +17,7 @@ require('gitsigns').setup({
     follow_files = true,
   },
   attach_to_untracked = true,
-  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = false,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
@@ -28,7 +28,7 @@ require('gitsigns').setup({
   current_line_blame_formatter_opts = {
     relative_time = true,
   },
-  sign_priority = 6,
+  sign_priority = 0,
   update_debounce = 100,
   status_formatter = nil, -- Use default
   max_file_length = 40000,
@@ -44,8 +44,3 @@ require('gitsigns').setup({
     enable = false,
   },
 })
-
--- vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg='#91b859' })
--- vim.api.nvim_set_hl(0, 'GitSignsChange', { fg='#1e66f5' })
--- vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg='#e53935' })
-
--- nvim/lua/plugins/appearence/statuscol.lua
+++ nvim/lua/plugins/appearence/statuscol.lua
@@ -3,8 +3,24 @@ local builtin = require("statuscol.builtin")
 require("statuscol").setup({
   relculright = true,
   segments = {
-    { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+    { text = { builtin.foldfunc }, maxwidth = 2, colwidth = 2, click = "v:lua.ScFa" },
     { text = { "%s" }, click = "v:lua.ScSa" },
+    -- {
+    --   sign = { namespace = { "gitsign" }, maxwidth = 1, colwidth = 1 },
+    --   click = "v:lua.ScSa"
+    -- },
+    {
+      sign = { name = { "Diagnostic" }, maxwidth = 0, auto = true },
+      click = "v:lua.ScSa"
+    },
+    {
+      sign = { name = { "Dap*" }, maxwidth = 2, auto = true },
+      click = "v:lua.ScSa"
+    },
+    {
+      sign = { name = { ".*" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
+      click = "v:lua.ScSa"
+    },
     { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
   },
 })
