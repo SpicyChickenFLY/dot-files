6fec91c3aeda900191c6031f66239395883b6065
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-03-13 17:47:19 +0800

    nvim: update

--- nvim/lua/plugins/tools/floaterm.lua
+++ nvim/lua/plugins/tools/floaterm.lua
@@ -10,7 +10,7 @@ return {
         g.floaterm_height = 0.99
         g.floaterm_title = '[' .. title .. ']:($1/$2)'
         g.floaterm_borderchars = '─│─│╭╮╯╰'
-        g.floaterm_opener = 'vsplit'
+        g.floaterm_opener = 'edit', -- 'vsplit'
 
         vim.cmd(('hi FloatBorder guibg=None')) -- terminal highlights
         vim.cmd('hi! link FloatermBorder FloatBorder')
