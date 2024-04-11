d01d1cf11a2131bd6a12e70da2eab249e439f8dd
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-04-11 11:09:51 +0800

    nvim: disable autocomplete in java

--- nvim/lua/plugins/coding/nvim-cmp.lua
+++ nvim/lua/plugins/coding/nvim-cmp.lua
@@ -73,6 +73,8 @@ return {
 
     cmp.setup.filetype('TelescopePrompt', { enabled = false, })
 
+    cmp.setup.filetype('java', { completion = { autocomplete = false } })
+
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
