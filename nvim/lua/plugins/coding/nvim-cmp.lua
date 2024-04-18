3482ed22e4cc39b0964ae557a7966e660750bf58
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-04-18 23:01:19 +0800

    nvim: disallow autocompelete for everything

--- nvim/lua/plugins/coding/nvim-cmp.lua
+++ nvim/lua/plugins/coding/nvim-cmp.lua
@@ -26,7 +26,7 @@ return {
         expand = function(args) snippet.lsp_expand(args.body) end,
       },
       preselect = 'none',
-      completion = { completeopt = 'menu, preview, menuone, noinsert', autocomplete = true },
+      completion = { completeopt = 'menu, preview, menuone, noinsert', autocomplete = false },
       mapping = require('core.keymaps').cmp_mapping(cmp, snippet, has_words_before),
       window = {
         completion = {
