27680fa868ab7ab3f0db5c7518824030a20feefc
SpicyChickenFLY <xixiayuchao@gmail.com>
2024-07-08 09:48:26 +0800

    nvim: remove noused plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -33,9 +33,8 @@ local plugins = {
   -------------- Language --------------
   { "mfussenegger/nvim-jdtls" },              -- Java LSP Tool
   { "nanotee/sqls.nvim" },                    -- SQL LSP Tool
-  require("plugins.language.sqlua"), -- Markdown preview
   require("plugins.language.markdown-preview"), -- Markdown preview
-  require("plugins.language.flutter-tools"),    -- Flutter LSP Tool
+  -- require("plugins.language.flutter-tools"),    -- Flutter LSP Tool
   -- require("plugins.language.nvim-java"),         -- general DAP debug configuration
   -- require("plugins.language.rest"),             -- HTTP request client
   -------------- Finder --------------
--- nvim/lua/plugins/language/sqlua.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "xemptuous/sqlua.nvim",
-  lazy = true,
-  cmd = { "SQLua" },
-  keys = require("core.keymaps")["sqlua"],
-  config = function() require('sqlua').setup() end
-}
